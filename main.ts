89a1e07635034f196c0dd379b5f8a2208d4e3287
Ryan Thomson <git@ryanthomson.net>
2024-06-02 10:58:28 -0400

    Add HOSTNAME and PORT envs

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 import app from './src/app.tsx';
 
-Deno.serve(app.fetch);
+const hostname = Deno.env.get('HOSTNAME') || undefined;
+const port = +(Deno.env.get('PORT') ?? '') || undefined;
+
+Deno.serve({ hostname, port }, app.fetch);
