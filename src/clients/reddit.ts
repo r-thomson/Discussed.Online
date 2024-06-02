c2f83ab4c362466b661aca34a43f7a21f911cc22
Ryan Thomson <git@ryanthomson.net>
2024-06-02 11:49:08 -0400

    Add extra error logging

--- src/clients/reddit.ts
+++ src/clients/reddit.ts
@@ -29,7 +29,10 @@ export async function searchLinks(
 	}
 
 	const response = await fetch(url);
-	if (response.status !== 200) throw Error(response.statusText);
+	if (response.status !== 200) {
+		console.error(await response.text());
+		throw Error(response.statusText);
+	}
 
 	return await response.json() as Results;
 }
