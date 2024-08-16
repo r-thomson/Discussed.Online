0869e2bd37c9fdf58977f00290e86d544de543b2
Ryan Thomson <git@ryanthomson.net>
2024-08-15 21:23:59 -0400

    Improve fault-tolerance of getDiscussions()

--- src/discussions.ts
+++ src/discussions.ts
@@ -1,5 +1,5 @@
-import { searchLinks as searchReddit } from './clients/reddit.ts';
 import { searchStories as searchHackerNews } from './clients/hacker-news.ts';
+import { searchLinks as searchReddit } from './clients/reddit.ts';
 
 export interface Discussion {
 	siteName: string;
@@ -11,7 +11,7 @@ export interface Discussion {
 
 const TWITTER_SITES = ['twitter.com', 'x.com'];
 
-function getRedditSearch(url: URL): string {
+function makeRedditQuery(url: URL): string {
 	if (url.hostname === 'www.youtube.com' && url.searchParams.get('v')) {
 		const v = url.searchParams.get('v');
 		return `url:${v} (site:youtube.com OR site:youtu.be)`;
@@ -31,7 +31,7 @@ function getRedditSearch(url: URL): string {
 	return 'url:' + url.hostname + url.pathname + url.search;
 }
 
-function getHackerNewsSearch(url: URL): string {
+function makeHackerNewsQuery(url: URL): string {
 	if (url.hostname === 'www.youtube.com' && url.searchParams.get('v')) {
 		const v = url.searchParams.get('v');
 		return `youtube.com/watch?v=${v}`;
@@ -50,32 +50,41 @@ function getHackerNewsSearch(url: URL): string {
 }
 
 export async function getDiscussions(searchUrl: URL): Promise<Discussion[]> {
-	const discussions: Discussion[] = await Promise.all([
-		searchReddit(getRedditSearch(searchUrl), {
-			sort: 'comments',
-		}).then((data) =>
-			data.data.children.map((child) => ({
-				siteName: child.data.subreddit_name_prefixed,
-				title: child.data.title,
-				url: 'https://www.reddit.com' + child.data.permalink,
-				score: child.data.score,
-				numComments: child.data.num_comments,
-			}))
-		),
-		searchHackerNews(getHackerNewsSearch(searchUrl), {
-			restrictSearchableAttributes: 'url',
-			ordering: 'popularity',
-		}).then((data) =>
-			data.hits.map((hit) => ({
-				siteName: 'Hacker News',
-				title: hit.title,
-				url: 'https://www.hackerneue.com/item?id=' + hit.objectID,
-				score: hit.points,
-				numComments: hit.num_comments,
-			}))
-		),
-	]).then((array) => array.flat(1));
+	const redditPromise = searchReddit(
+		makeRedditQuery(searchUrl),
+		{ sort: 'comments' },
+	).then((data) =>
+		data.data.children.map((child) => ({
+			siteName: child.data.subreddit_name_prefixed,
+			title: child.data.title,
+			url: 'https://www.reddit.com' + child.data.permalink,
+			score: child.data.score,
+			numComments: child.data.num_comments,
+		}))
+	);
 
-	discussions.sort((a, b) => b.numComments - a.numComments);
+	const hackerNewsPromise = searchHackerNews(
+		makeHackerNewsQuery(searchUrl),
+		{ restrictSearchableAttributes: 'url', ordering: 'popularity' },
+	).then((data) =>
+		data.hits.map((hit) => ({
+			siteName: 'Hacker News',
+			title: hit.title,
+			url: 'https://www.hackerneue.com/item?id=' + hit.objectID,
+			score: hit.points,
+			numComments: hit.num_comments,
+		}))
+	);
+
+	const results = await Promise.allSettled([
+		redditPromise,
+		hackerNewsPromise,
+	]);
+
+	const discussions: Discussion[] = results
+		.filter((result) => result.status === 'fulfilled')
+		.flatMap((result) => result.value)
+		.sort((a, b) => b.numComments - a.numComments);
+
 	return discussions;
 }
