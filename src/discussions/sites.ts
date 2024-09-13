c4c119689ffeb20b4d8e0b8b008089233b46a9b7
Ryan Thomson <git@ryanthomson.net>
2024-09-12 21:09:38 -0400

    Rename hacker-news.ts

--- src/discussions/sites.ts
+++ src/discussions/sites.ts
@@ -1,3 +1,3 @@
-export { default as HackerNews } from './hacker-news.ts';
+export { default as HackerNews } from './hacker_news.ts';
 export { default as Lobsters } from './lobsters.ts';
 export { default as Reddit } from './reddit.ts';
