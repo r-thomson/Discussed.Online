e9ab359fc8f8ee00b08ba168ff84b0e500988d65
Ryan Thomson <git@ryanthomson.net>
2025-06-08 21:10:31 -0400

    Add error pages

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { TrieRouter } from 'hono/router/trie-router';
 import { trimTrailingSlash } from 'hono/trailing-slash';
 import { serveAssets } from './assets.ts';
 import Document from './components/Document.tsx';
+import { Error404, Error500 } from './components/errors.tsx';
 import { settingsMiddleware } from './middleware/settings.ts';
 import healthz from './routes/healthz.ts';
 import index from './routes/index.ts';
@@ -18,6 +19,18 @@ app.use(logger());
 app.use(trimTrailingSlash());
 app.use(jsxRenderer(Document));
 app.use(settingsMiddleware);
+
+app.notFound(async (c) => {
+	c.status(404);
+	return c.render(Error404());
+});
+
+app.onError(async (err, c) => {
+	console.error(err);
+
+	c.status(500);
+	return c.render(Error500());
+});
 
 app.route('/', index);
 app.route('/settings', settings);
---
+++ src/components/errors.tsx
@@ -0,0 +1,13 @@
+export const Error404 = () => (
+	<div class='max-w-2xl mx-auto px-4 sm:px-8 py-32'>
+		<pre class='text-xl font-medium text-gray-800'>:\</pre>
+		<div class='text-lg mt-2'>Not Found</div>
+	</div>
+);
+
+export const Error500 = () => (
+	<div class='max-w-2xl mx-auto px-4 sm:px-8 py-32'>
+		<pre class='text-xl font-medium text-gray-800'>D:</pre>
+		<div class='text-lg mt-2'>Internal Server Error</div>
+	</div>
+);
